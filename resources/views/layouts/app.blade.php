dbe27b10d5940de559c9f71c8a9401a0ad39317a
Baboo81 <magicbaboo@gmail.com>
2025-10-03 16:26:14 +0200

    Installation de base

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        @include('partials.header')
+    </head>
+    <body>
+        <!-- Conteneur pour Klaro (bannière modal) -->
+        <div id="Klaro"></div>
+
+        <header>
+            @include('partials.nav')
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            @include('partials.footer')
+        </footer>
+    </body>
+</html>
