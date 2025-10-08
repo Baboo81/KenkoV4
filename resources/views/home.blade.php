666c7103bb051c90d5476bd80e645a4aa00e7fc2
Baboo81 <magicbaboo@gmail.com>
2025-10-08 19:40:24 +0200

    home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,18 @@
 {{-- Div animation : progression de la page END --}}
 
 {{-- Section : Banner --}}
-
+<section class="home">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mainContent">
+                <h1 class="mainTitle"></h1>
+                <span class="secondTitle text-nowrap">Bienvenue dans deux univers ...</span>
+                <div class="logoContainer">
+                    <img src="./assets/img/logo/logoAnimate.svg" alt="Logo animé" id="logoAnimation">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 {{-- Section : Banner END --}}
 @endsection
