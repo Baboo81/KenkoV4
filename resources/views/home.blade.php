b7521ae28a451ed44fb4c79ff573fbadbed1e205
Baboo81 <magicbaboo@gmail.com>
2025-10-09 16:42:09 +0200

    wave

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,65 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- Div animation : progression de la page --}}
-<div id="scroll-indicator">
-    <div class="inner-circle">
-        <div class="scroll-arrow"></div>
+    {{-- Div animation : progression de la page --}}
+    <div id="scroll-indicator">
+        <div class="inner-circle">
+            <div class="scroll-arrow"></div>
+        </div>
     </div>
-</div>
-{{-- Div animation : progression de la page END --}}
+    {{-- Div animation : progression de la page END --}}
 
-{{-- Section : Banner --}}
-<section class="home">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 mainContent">
-                <h1 class="mainTitle">
-                    <span class="secondTitle text-nowrap">
-                        {{ $homeData['home'] }}
-                    </span>
-                </h1>
-                <div class="logoContainer">
-                    <img src="{{ asset('assets/img/logo/logoKenko2.png') }}" alt="Logo animé" id="logoAnimation">
+    {{-- Section : Banner --}}
+    <section class="home">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mainContent">
+                    <h1 class="mainTitle">
+                        <span class="secondTitle text-nowrap">
+                            {{ $homeData['home'] }}
+                        </span>
+                    </h1>
+                    <div class="logoContainer">
+                        <img src="{{ asset('assets/img/logo/logoKenko2.png') }}" alt="Logo animé" id="logoAnimation">
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-{{-- Section : Banner END --}}
+    </section>
+    {{-- Section : Banner END --}}
 
-{{-- Section : Cards (Kenko-Ho & Kenko-web) --}}
-<section class="mainSection">
-    <div class="container mw-100">
-        <div class="row">
-            @foreach ($homeData['cards'] as $card)
-                <div class="col-md-12 {{ $card['title'] === 'KENKO-HO' ? 'part1' : 'part2' }}">
-                    <div class="card rounded-4 {{ $card['title'] === 'KENKO-HO' ? 'bg-grey' : 'bg-blue' }} p-3" style="max-width: 55rem;">
-                        <div class="kenkoTitles">
-                            <h3 class="text-center my-5">{{ $card['title'] }}</h3>
-                        </div>
+    <!-- SVG WAVE -->
+    <svg class="wave" viewBox="0 0 1440 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true" role="img">
+        <path d="M0,32 C200,120 400,0 720,48 C1040,96 1240,0 1440,48 L1440 120 L0 120 Z" fill="#bfc1c2"></path>
+    </svg>
 
-                        <img src="" class="card-img-top rounded-3" alt="Image représentant un jardin zen">
+    {{-- Section : Cards (Kenko-Ho & Kenko-web) --}}
+    <section class="mainSection">
+        <div class="container mw-100">
+            <div class="row">
+                @foreach ($homeData['cards'] as $card)
+                    <div class="col-md-12 {{ $card['title'] === 'KENKO-HO' ? 'part1' : 'part2' }}">
+                        <div class="card rounded-4 {{ $card['title'] === 'KENKO-HO' ? 'bg-grey' : 'bg-blue' }} p-3"
+                            style="max-width: 55rem;">
+                            <div class="kenkoTitles">
+                                <h3 class="text-center my-5">{{ $card['title'] }}</h3>
+                            </div>
 
-                        <div class="card-body d-flex flex-column">
-                            <h5 class="card-title fs-1 text-center mb-5">{{ $card['title2'] }}</h5>
-                            <p class="card-text text-center">{!! $card['p1'] !!}</p>
+                            <img src="" class="card-img-top rounded-3" alt="Image représentant un jardin zen">
 
-                            @if (!empty($card['p2']))
-                                <p class="fontGreen text-center fw-bold my-3">{!! $card['p2'] !!}</p>
-                            @endif
+                            <div class="card-body d-flex flex-column">
+                                <h5 class="card-title fs-1 text-center mb-5">{{ $card['title2'] }}</h5>
+                                <p class="card-text text-center">{!! $card['p1'] !!}</p>
 
-                            @if ($card['title'] === 'KENKO-WEB')
-                                <div class="text-center my-5">
-                                    <button onclick="window.location.href='{{ url('/kenko-web') }}';" class="button">Visiter Kenko-Web</button>
-                                </div>
-                            @endif
+                                @if (!empty($card['p2']))
+                                    <p class="fontGreen text-center fw-bold my-3">{!! $card['p2'] !!}</p>
+                                @endif
+
+                                @if ($card['title'] === 'KENKO-WEB')
+                                    <div class="text-center my-5">
+                                        <button onclick="window.location.href='{{ url('/kenko-web') }}';"
+                                            class="button">Visiter Kenko-Web</button>
+                                    </div>
+                                @endif
+                            </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-    </div>
-</section>
-{{-- Section : Cards (Kenko-Ho & Kenko-web) END--}}
+    </section>
+    {{-- Section : Cards (Kenko-Ho & Kenko-web) END --}}
 @endsection
