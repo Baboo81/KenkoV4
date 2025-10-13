f70ca702b94c93489c422dccf553fd149019d68a
Baboo81 <magicbaboo@gmail.com>
2025-10-13 11:55:57 +0200

    kenko-web.blade.php

--- resources/data/kenkoWebData.php
+++ resources/data/kenkoWebData.php
@@ -5,4 +5,22 @@ return [
         'main_Title' => "Kenko-Web",
         'sub_Title' => "Besoin d'un site web professionnel ?",
     ],
+    'purposes' => [
+        'main_Title' => "Ce que votre webmaster vous propose",
+        'article1' => [
+            'sub_Title' => "Un accompagnement de qualité",
+            'txt' => "Je suis à votre écoute et je vous conseille tout au long de la vie du site",
+            'img' => "/assets/img/kenkoWeb/pictos/accompagnement.svg",
+        ],
+        'article2' => [
+            'sub_Title' => "La liberté de choix",
+            'txt' => "Votre site web vous appartient, modifiez-le à votre guise ou confiez-moi cette mission !",
+            'img' => "/assets/img/kenkoWeb/pictos/liberté.svg",
+        ],
+        'article3' => [
+            'sub_Title' => "Un site sur-mesure",
+            'txt' => "Je crée un site qui vous correspond",
+            'img' => "/assets/img/kenkoWeb/pictos/surMesure.svg",
+        ],
+    ],
 ];
--- resources/views/kenko-web.blade.php
+++ resources/views/kenko-web.blade.php
@@ -27,4 +27,40 @@
         </div>
     </section>
     {{-- Section : Banner END--}}
+
+      <!-- SVG WAVE -->
+    <svg class="wave" viewBox="0 0 1440 120" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true" role="img">
+        <path d="M0,32 C200,120 400,0 720,48 C1040,96 1240,0 1440,48 L1440 120 L0 120 Z" fill="#bfc1c2"></path>
+    </svg>
+
+
+    {{-- Section : Purposes --}}
+    <section class="purposes">
+        <div class="containter">
+            <div class="row">
+                <div class="my5">
+                    <h1 class="text-center">
+                        {{ $kenkoWebData['purposes']['main_Title'] ?? '' }}
+                    </h1>
+                </div>
+                @foreach(['article1', 'article2', 'article3'] as $tempPurposes)
+                    @php $articles = $kenkoWebData['purposes'][$tempPurposes] @endphp
+                    <article class="col-xl-4 col-xxl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 text-center">
+                        <img src="{{ asset($articles['img']) ?? '' }} " alt="Pictos représentant les différentes propositions du suivi">
+                        <h4>
+                            {{ $articles['sub_Title'] ?? '' }}
+                        </h4>
+                        <p>
+                            {{ $articles['txt'] ?? '' }}
+                        </p>
+                    </article>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    {{-- Section : Purposes END--}}
+
+    {{-- Section : Offers  --}}
+    {{-- Section : Offers  END--}}
 @endsection
