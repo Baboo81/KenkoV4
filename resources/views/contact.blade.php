33ce51584f33bdf305c221fd1ee2374625d29a12
Baboo81 <magicbaboo@gmail.com>
2025-10-17 10:43:36 +0200

    mise en forme

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <section class="contact my-5">
     <div class="container">
-        <h1 class="text-center text-muted mb-5 titleH1">{{ $contactData['title'] }}</h1>
+        <h1 class="text-center text-muted mb-5 titleH1">
+            {{ $contactData['title'] }}
+        </h1>
 
         {{-- Messages flash --}}
         @foreach (['success' => 'success', 'error' => 'danger'] as $key => $type)
@@ -36,7 +38,10 @@
                         <div class="mb-3">
                             <label for="{{ $field['id'] }}" class="form-label fw-semibold">{{ $field['label'] }}</label>
                             <div class="input-group">
-                                <span class="input-group-text"><i class="bi {{ $field['icon'] }}"></i></span>
+                                <span class="input-group-text">
+                                    <i class="bi {{ $field['icon'] }}">
+                                    </i>
+                                </span>
                                 <input type="{{ $field['type'] }}"
                                        class="form-control"
                                        id="{{ $field['id'] }}"
@@ -61,18 +66,23 @@
                                       name="{{ $contactData['message_field']['id'] }}"
                                       rows="5"
                                       placeholder="{{ $contactData['message_field']['placeholder'] }}"
-                                      required></textarea>
+                                      required>
+                            </textarea>
                         </div>
                     </div>
 
                     <div class="text-center my-5">
-                        <button type="submit" class="button">{{ $contactData['buttons']['submit'] }}</button>
+                        <button type="submit" class="button">
+                            {{ $contactData['buttons']['submit'] }}
+                        </button>
                     </div>
                 </form>
 
                 <div class="text-center mt-4">
                     <a href="{{ url('/') }}" class="text-decoration-none">
-                        <button class="button" type="button">{{ $contactData['buttons']['back'] }}</button>
+                        <button class="button" type="button">
+                            {{ $contactData['buttons']['back'] }}
+                        </button>
                     </a>
                 </div>
             </div>
